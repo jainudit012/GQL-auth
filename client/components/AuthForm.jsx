5894f78c2ac235c538d1cb7d8a31defaee6376d2
Udit Jain <udit.jain@opscale.io>
2020-04-30 17:53:17 +0530

    add authform skeleton

---
+++ client/components/AuthForm.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class AuthForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+
+    render() { 
+        return ( 
+            <div className="row">
+                <form className="col s8">
+                    <div className="input-field">
+                        <label>Email</label>
+                        <input type="email" value={this.state.email} onChange={e => this.setState({email: e.target.value})}/>
+                    </div>
+                    <div className="input-field">
+                        <label>Password</label>
+                        <input type="password" value={this.state.password} onChange={e=> this.setState({password: e.target.value})}/>
+                    </div>
+                    <button className="btn">Sumbmit</button>
+                </form>
+            </div>
+         );
+    }
+}
+ 
+export default AuthForm;
