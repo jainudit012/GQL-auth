c88fd3af04d801e2708aafe986a4bfe78ede04f3
Udit Jain <udit.jain@opscale.io>
2020-04-30 14:12:31 +0530

    add logout mutation

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -16,7 +16,15 @@ const mutation = new GraphQLObjectType({
                 }
             },
             resolve(parentValue, {email, password}, req){
-                AuthService.signup({email, password, req})
+                return AuthService.signup({email, password, req})
+            }
+        },
+        logout: { 
+            type: UserType,
+            resolve(parentValue, args, req){
+                const { user } = req
+                req.logout()
+                return user
             }
         }
     }
