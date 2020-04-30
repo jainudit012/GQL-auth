8af0fa53b2d58b63a4971c85eb44393859d00bc4
Udit Jain <udit.jain@opscale.io>
2020-04-30 14:14:47 +0530

    add login mutation

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -26,6 +26,20 @@ const mutation = new GraphQLObjectType({
                 req.logout()
                 return user
             }
+        },
+        login: { 
+            type: UserType,
+            args: {
+                email: {
+                    type: GraphQLString
+                },
+                password: {
+                    type: GraphQLString
+                }
+            },
+            resolve(parentValue, {email, password}, req){
+                return AuthService.login({email, password, req})
+            }
         }
     }
 })
