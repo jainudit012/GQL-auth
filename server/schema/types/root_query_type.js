b37ab7f1f609badded7a41faa556f9c0c14036c4
Udit Jain <udit.jain@opscale.io>
2020-04-30 14:18:17 +0530

    add current user in root query

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,10 +1,14 @@
-const { GraphQLObjectType, GraphQLID } = require('graphql');
+const { GraphQLObjectType } = require('graphql');
+const UserType = require('./user_type')
 
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    dummyField: { 
-      type: GraphQLID
+    user: { 
+      type: UserType,
+      resolve(parentValue, args, req){
+        return req.user
+      }
     }
   }
 });
