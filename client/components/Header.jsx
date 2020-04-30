3e1d4fcb4600e10d3d837335d59a8eac4f63314f
Udit Jain <udit.jain@opscale.io>
2020-04-30 17:25:53 +0530

    add Header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,12 +1,54 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo'
+import currentUserQuery from '../queries/currentUser'
+import logout from '../mutations/logout'
+import { Link } from 'react-router-dom'
 
 class Header extends Component {
+
+    onLogout(){
+        this.props.mutate({
+            refetchQueries: [{query: currentUserQuery}]
+        })
+    }
+
+    renderButtons(){
+        const {loading, user} = this.props.data
+        
+        if(loading) return <div />
+        if(user){
+            return (
+                <li>
+                    <a onClick={() => this.onLogout()}>Log Out</a>
+                </li>
+            )
+        }else {
+            return (
+                <div>
+                    <li>
+                        <Link to="/signup">Sign Up</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Log In</Link>
+                    </li>
+                </div>
+            )
+        }
+    }
     
-    render() { 
+    render() {
         return ( 
-            <div>Header</div>
+            <nav>
+                <div className="nav-container">
+                    <Link to="/" className="brand-logo left">Home</Link>
+                    <ul className="right">
+                        {this.renderButtons()}
+                    </ul>
+                </div>
+            </nav>
          );
     }
 }
  
-export default Header;
+export default graphql(logout)(
+    graphql(currentUserQuery)(Header));
