543d2145bb2994cc949bae97db825de8552c3885
Udit Jain <udit.jain@opscale.io>
2020-05-01 17:54:24 +0530

    add dashboard component

---
+++ client/components/Dashboard.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Dashboard = props => {
+    return <h2>Dashboard</h2>;
+}
+ 
+export default Dashboard;
--- client/index.js
+++ client/index.js
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import AppComponent from './components/App'
 import LoginForm from './components/LoginForm'
 import SignupForm from './components/SignupForm'
+import DashBoard from './components/Dashboard'
 
 const client = new ApolloClient({
   dataIdFromObject: o => o.id
@@ -21,6 +22,7 @@ const Root = () => {
             <Route path="/" component={AppComponent} exact />
             <Route path="/login" component={LoginForm} exact />
             <Route path="/signup" component={SignupForm} exact />
+            <Route path="/dashboard" component={DashBoard} exact />
           </Header>
         </Switch>
       </HashRouter>
