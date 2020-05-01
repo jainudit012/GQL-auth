b1647df2a3850048e04b3497083ade0869712a67
Udit Jain <udit.jain@opscale.io>
2020-05-01 17:10:23 +0530

    add signup functionality

---
+++ client/components/SignupForm.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import AuthForm from './AuthForm'
+import { graphql } from 'react-apollo'
+import SignupMutation from '../mutations/signup'
+import currentUserQuery from '../queries/currentUser'
+
+class SignupForm extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            errors: []
+        }
+    }
+
+    onFormSubmit({email, password}) {
+        this.props.mutate({
+            variables: {email, password},
+            refetchQueries: [{ query: currentUserQuery}]
+        })
+        .then(()=> {
+            this.setState({errors: []})
+        })
+        .catch(err => {
+            const errors = err.graphQLErrors.map(err=>  err.message)
+            this.setState({errors})
+        })
+    }
+
+
+    render() { 
+        return ( 
+            <div className="container">
+                <h3>Signup Form</h3>
+                <AuthForm 
+                    onSubmit={({email, password}) => this.onFormSubmit({email, password})}
+                    errors={this.state.errors}/>
+            </div>
+         );
+    }
+}
+ 
+export default graphql(SignupMutation)(
+    graphql(currentUserQuery)(SignupForm));
--- client/index.js
+++ client/index.js
@@ -6,6 +6,7 @@ import { ApolloProvider } from 'react-apollo'
 import Header from './components/Header'
 import AppComponent from './components/App'
 import LoginForm from './components/LoginForm'
+import SignupForm from './components/SignupForm'
 
 const client = new ApolloClient({
   dataIdFromObject: o => o.id
@@ -19,6 +20,7 @@ const Root = () => {
           <Header> 
             <Route path="/" component={AppComponent} exact />
             <Route path="/login" component={LoginForm} exact />
+            <Route path="/signup" component={SignupForm} exact />
           </Header>
         </Switch>
       </HashRouter>
